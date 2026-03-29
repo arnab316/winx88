6e0e718b750da4b55b6da0e082be09241fad15e8
Arnab <arnabgolder7@gmail.com>
2026-03-29 12:33:27 +0530

    next +

--- src/auth/auth.gateway.ts
+++ src/auth/auth.gateway.ts
@@ -2,36 +2,45 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  ConnectedSocket,
 } from '@nestjs/websockets';
 import { AuthService } from './auth.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: { origin: '*' },
 })
-export class AuthGateway {
-  constructor(private authService: AuthService) { 
+export class AuthGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(private authService: AuthService) {}
+
+  handleConnection(@ConnectedSocket() client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(@ConnectedSocket() client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
   }
 
   @SubscribeMessage('checkUsername')
-  async handleCheckUsername(@MessageBody() username: string) {
+  async handleCheckUsername(@MessageBody() username: string,
+  @ConnectedSocket() client: Socket,
+) {
     if (!username || username.length < 3) {
-      return {
-        event: 'usernameResult',
-        data: {
-          available: false,
-          message: 'Too short',
-        },
-      };
+      client.emit('usernameResult', {
+        username,
+        available: false,
+        message: 'Too short',
+      });
+      return;
     }
 
     const isTaken = await this.authService.isUsernameTaken(username);
 
-    return {
-      event: 'usernameResult',
-      data: {
-        username,
-        available: !isTaken,
-      },
-    };
+     client.emit('usernameResult', {
+      username,
+      available: !isTaken,
+    });
   }
 }
