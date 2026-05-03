6eba0575c6cd9692fb8ecb9d030fb2431c4bcc0f
Arnab <arnabgolder7@gmail.com>
2026-05-03 22:36:55 +0530

    added jack[ot

--- data-source.ts
+++ data-source.ts
@@ -1,12 +1,14 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
+import 'dotenv/config';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
 
-export const AppDataSource = new DataSource({
-  type: process.env.DB_TYPE as any ,
+
+export default new DataSource({
+  type: 'postgres',
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT as any ,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT || 5432),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS?.trim() || '',
   database: process.env.DB_NAME,
-  migrations: ["src/migrations/*.ts"],
+  migrations: ['src/migrations/*.ts'],
 });
